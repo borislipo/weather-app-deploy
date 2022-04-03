06c210182e86d96c7be2945339f786bf68e8eb72
boris <boris.lipo123@gmail.com>
2022-04-03 17:15:43 +0300

    second commit

--- src/api/config.js
+++ src/api/config.js
@@ -1,3 +1,3 @@
-export const apiKey = 'TAAZo2oqjDQGCxoE4hZNMO68rQWM4fHe';
+export const apiKey = 'oVccWusILDv0J4WdylX8Ek2H4HmAsLhn';
 export const url = 'https://dataservice.accuweather.com';
 export const telAvivKey = '215854';
